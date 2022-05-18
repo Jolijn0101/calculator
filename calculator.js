628b2ce7da1afa6952763a7f44680b7b36329e49
Jolijn Rosendaal <84015050+Jolijn0101@users.noreply.github.com>
2022-05-18 10:44:12 +0200

    calculates  string

--- calculator.js
+++ calculator.js
@@ -1,21 +1,57 @@
 let sum = '0';
-let answer = Number(sum);
+let formula;
 
-const myScript = (number) => {
+const addNumber = (number) => {
+  if (sum.length >= 15) {
+    return null;
+  }
+
   if (sum === '0') {
     sum = number.toString();
+    formula = number.toString();
   } else {
     sum = sum + number.toString();
+    formula = formula + number.toString();
   }
   document.getElementById('screen').innerHTML = sum;
 };
+
+const addOperator = (operator) => {
+  if (sum.length >= 15) {
+    return null;
+  }
+
+  if (sum === '0') {
+    sum = operator;
+    formula = operator;
+  } else if (operator === 'x') {
+    sum = sum + 'x';
+    formula = formula + '*';
+  } else if (operator === '÷') {
+    sum = sum + '÷';
+    formula = formula + '/';
+  } else {
+    sum = sum + operator;
+    formula = formula + operator;
+  }
+  document.getElementById('screen').innerHTML = sum;
+};
+
+const negativeNum = () => {};
 
 const Clear = () => {
   sum = '0';
   document.getElementById('screen').innerHTML = sum;
 };
 
-const callAnswer = () => {
-  console.log(answer);
-  document.getElementById('screen').innerHTML = answer;
+const Answer = () => {
+  try {
+    let answer = eval(formula);
+    sum = answer.toString();
+    document.getElementById('screen').innerHTML = sum;
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      document.getElementById('screen').innerHTML = 'Error';
+    }
+  }
 };
