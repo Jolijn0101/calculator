8b78395f09a5715346ded4a8264008983f39aa95
Jolijn Rosendaal <84015050+Jolijn0101@users.noreply.github.com>
2022-05-18 14:09:46 +0200

    calculates percentages

--- calculator.js
+++ calculator.js
@@ -20,20 +20,44 @@ const addOperator = (operator) => {
   if (sum.length >= 15) {
     return null;
   }
-
-  if (sum === '0') {
+  if (sum === '0' && operator === '-') {
     sum = operator;
     formula = operator;
+  } else if (sum == '0' && operator !== '-') {
+    return null;
   } else if (operator === 'x') {
     sum = sum + 'x';
     formula = formula + '*';
   } else if (operator === '÷') {
     sum = sum + '÷';
     formula = formula + '/';
+  } else if (operator === ',') {
+    sum = sum + ',';
+    formula = formula + '.';
   } else {
     sum = sum + operator;
     formula = formula + operator;
   }
+  document.getElementById('screen').innerHTML = sum;
+};
+
+const operatorSearch = () => {
+  const operatorArr = ['-', '+', '/', '*'];
+  let lastOperator = 0;
+  for (let index = 0; index < operatorArr.length; index++) {
+    let search = formula.lastIndexOf(operatorArr[index]);
+    if (search >= lastOperator) {
+      lastOperator = search;
+    }
+  }
+  return lastOperator;
+};
+
+const Percentage = () => {
+  let numIndex = operatorSearch() + 1;
+  let percentage = formula.slice(numIndex) / 100;
+  formula = formula.slice(0, numIndex) + percentage;
+  sum = sum.slice(0, numIndex) + percentage;
   document.getElementById('screen').innerHTML = sum;
 };
 
